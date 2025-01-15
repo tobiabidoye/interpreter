a11fdc793be90295bced0f4a9378312730a7131d
Tobi Abidoye <tobiabidoye14@gmail.com>
2025-01-15 15:26:45 -0500

    fixed lexer bugs added test cases

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -5,26 +5,96 @@ import(
     "interpreter/token"
 )
 
-func testNextToken(t * testing.T){
-    input := `=+(){},;`
+func TestNextToken(t * testing.T){
+    input :=  `let five = 5;
+    let ten = 10;
+
+    let add = fn(x, y) {
+      x + y;
+    };
+
+    let result = add(five, ten);
+    !-/*5;
+    5 < 10 > 5;
+
+    if (5 < 10) {
+        return true;
+    } else {
+        return false;
+    }`
     tests := []struct{
         expectedType token.TokenType
         expectedLiteral string
 
     }{
+
+        {token.LET, "let"},
+        {token.IDENT, "five"},
         {token.ASSIGN, "="},
-        {token.PLUS, "+"},
-        {token.LPAREN, "("},
-        {token.RPAREN, ")"}, 
-        {token.LBRACE, "{"},
-        {token.RBRACE, "}"},
-        {token.COMMA, ","},
+        {token.INT, "5"},
         {token.SEMICOLONS, ";"},
+        {token.LET, "let"},
+        {token.IDENT, "ten"},
+        {token.ASSIGN, "="},
+        {token.INT, "10"},
+        {token.SEMICOLONS, ";"},
+        {token.LET, "let"},
+        {token.IDENT, "add"},
+        {token.ASSIGN, "="},
+        {token.FUNCTION, "fn"},
+        {token.LPAREN, "("},
+        {token.IDENT, "x"},
+        {token.COMMA, ","},
+        {token.IDENT, "y"},
+        {token.RPAREN, ")"},
+        {token.LBRACE, "{"},
+        {token.IDENT, "x"},
+        {token.PLUS, "+"},
+        {token.IDENT, "y"},
+        {token.SEMICOLONS, ";"},
+        {token.RBRACE, "}"},
+        {token.SEMICOLONS, ";"},
+        {token.LET, "let"},
+        {token.IDENT, "result"},
+        {token.ASSIGN, "="},
+        {token.IDENT, "add"},
+        {token.LPAREN, "("},
+        {token.IDENT, "five"},
+        {token.COMMA, ","},
+        {token.IDENT, "ten"},
+        {token.RPAREN, ")"},
+        {token.SEMICOLONS, ";"},
+        {token.BANG, "!"},
+        {token.MINUS,"-"},
+        {token.SLASH, "/"},
+        {token.ASTERIK, "*"},
+        {token.INT, "5"},
+		{token.SEMICOLONS, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLONS, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLONS, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLONS, ";"},
+		{token.RBRACE, "}"},
         {token.EOF, ""},
-
-
-
-    } 
+    }
 
     l := New(input)
     for i, tt := range tests{
--- token/token.go
+++ token/token.go
@@ -2,10 +2,65 @@ package token
 
 type TokenType string
 
-type token struct{
+type Token struct{
     Type TokenType
     Literal string
-
-
 }
 
+const(
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF" 
+
+
+    //identifiers and literals
+    IDENT = "IDENT"
+    INT = "INT"
+    
+    //Operators
+    ASSIGN = "="
+    PLUS = "+"
+    MINUS = "-"
+    BANG = "!"
+    ASTERIK = "*" 
+    SLASH = "/"
+
+    LT = "<" 
+    GT = ">"
+
+    //Delimiters
+    COMMA = ","
+    SEMICOLONS = ";"
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    //Keywords
+    FUNCTION = "FUNCTION"
+    LET = "LET"
+    TRUE = "TRUE" 
+    FALSE = "FALSE" 
+    IF = "IF"
+    ELSE = "ELSE"
+    RETURN = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+    "fn":FUNCTION,
+    "let":LET,
+    "true": TRUE, 
+    "false": FALSE,
+    "if": IF,
+    "else": ELSE,
+    "return": RETURN, 
+}
+
+func LookUpIdent(ident string) TokenType{
+    if tok, ok := keywords[ident]; ok{ //if identifier exists in the map execute the if statement
+       return tok; 
+    } 
+
+
+   return IDENT 
+
+}
